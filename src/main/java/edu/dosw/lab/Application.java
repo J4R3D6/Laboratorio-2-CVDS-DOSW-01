0fdfb40dbddb280b5a31beb2ee7efcb3c8b7e84f
Daniel Eduardo Useche <du.important@gmail.com>
2025-08-24 21:07:11 -0500

    Reto2 check

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,11 +1,12 @@
 package edu.dosw.lab;
 
 import edu.dosw.lab.Comportamentales.reto1.Reto1TiendaDeDonPepe;
+import edu.dosw.lab.Creacionales.reto2.Reto2ChefDeHamburguesas;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("✅ Proyecto Maven configurado y corriendo correctamente");
-        //Llamada reto 1
         Reto1TiendaDeDonPepe.ejecutar();
+        Reto2ChefDeHamburguesas.ejecutar();
     }
 }
