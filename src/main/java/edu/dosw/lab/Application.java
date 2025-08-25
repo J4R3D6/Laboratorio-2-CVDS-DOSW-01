2637d34e2ac05b840069694ca869e67653e3079b
Jared <Jared.farfan-g@mail.escuelaing.edu.co>
2025-08-24 21:49:03 -0500

    organizacion en carpetas

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -6,7 +6,7 @@ import edu.dosw.lab.Creacionales.reto2.Reto2ChefDeHamburguesas;
 public class Application {
     public static void main(String[] args) {
         System.out.println("✅ Proyecto Maven configurado y corriendo correctamente");
-        Reto1TiendaDeDonPepe.ejecutar();
+        //Reto1TiendaDeDonPepe.ejecutar();
         Reto2ChefDeHamburguesas.ejecutar();
     }
 }
